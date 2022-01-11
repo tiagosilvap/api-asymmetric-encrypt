6962154fc0e421c931d85916a6f5cfe0b6864cd8
Tiago Pereira <tiago.pereira@hotmart.com>
2022-01-11 18:59:33 -0300

    removendo serviços de produto

--- src/main/java/com/asymmetric/encrypt/repository/ConfigRepository.java
+++ src/main/java/com/asymmetric/encrypt/repository/ConfigRepository.java
@@ -1,7 +1,6 @@
 package com.asymmetric.encrypt.repository;
 
 import com.asymmetric.encrypt.model.Config;
-import com.asymmetric.encrypt.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
